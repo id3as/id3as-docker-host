4a55d942d2e572d5c6eba9ae42592242a23a0a3a
Rob Ashton <robashton@codeofrob.com>
2015-01-27 11:50:01 +0000

    Inception

---
+++ build-image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+cd ${0%/*}
+
+TMPDIR=$(mktemp -d /tmp/XXXXXXXX)
+CIDFILE=$TMPDIR/cid
+PWD=$(pwd)
+PWD_ON_HOST="/home/id3as"${PWD#$HOME}
+PROVISION_CMD="/host/provision-docker-host.sh"
+BASE_IMAGE="id3as-baseimage:0.0.1"
+IMAGE_NAME="docker-host"
+
+docker run --cidfile=$CIDFILE -t -v $PWD:/host:ro $BASE_IMAGE /bin/bash -c $PROVISION_CMD
+
+CID=$(cat $CIDFILE)
+docker rmi $IMAGE_NAME >/dev/null 2>&1 || echo -n
+IID=$(docker commit $CID $IMAGE_NAME)
+docker rm $CID >/dev/null 2>&1
---
+++ provision-docker-host.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+if [[ $UID -ne 0 ]]; then
+  echo "$0 must be run as root"
+  exit 1
+fi
+
+function package_exists {
+   return $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed")
+};
+
+function apt_install {
+  local package=$1
+  package_exists $package
+  if [[ $? -eq 1 ]]; then
+    echo "Skipping $@"
+  else
+    echo
+    echo Installing $@
+    echo
+    DEBIAN_FRONTEND=noninteractive apt-get -fqy install $@
+  fi
+};
+
+
+function update_sources {
+  echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+  apt-get update
+}
+
+function install_packages {
+  apt_install apt-transport-https
+  apt_install ca-certificates
+  apt_install lxc
+  apt_install iptables
+  apt_install lxc-docker
+};
+
+
+update_sources
+install_packages
+
+## Install the magic wrapper.
+#ADD ./wrapdocker /usr/local/bin/wrapdocker
+#RUN chmod +x /usr/local/bin/wrapdocker
+
