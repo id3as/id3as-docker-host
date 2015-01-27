d8af4dd76349ebb4158200049f4a338e2cf995c4
Rob Ashton <robashton@codeofrob.com>
2015-01-27 16:53:33 +0000

    Daemonise our custom docker wrapper

--- mount/provision-docker-host.sh
+++ mount/provision-docker-host.sh
@@ -5,11 +5,11 @@ if [[ $UID -ne 0 ]]; then
   exit 1
 fi
 
-function package_exists {
+package_exists() {
    return $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed")
 };
 
-function apt_install {
+apt_install() {
   local package=$1
   package_exists $package
   if [[ $? -eq 1 ]]; then
@@ -20,26 +20,31 @@ function apt_install {
     echo
     DEBIAN_FRONTEND=noninteractive apt-get -fqy install $@
   fi
-};
+}
 
 
-function update_sources {
+update_sources() {
   echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
   apt-get update
 }
 
-function install_packages {
+install_packages() {
   apt_install apt-transport-https
   apt_install ca-certificates
   apt_install lxc
   apt_install iptables
   apt_install lxc-docker
-};
+}
+
+install_daemon() {
+  cp -r /host/wrapdocker /etc/service/wrapdocker
+}
 
 
 update_sources
 install_packages
+install_daemon
 
 ## Install the magic wrapper.
 #ADD ./wrapdocker /usr/local/bin/wrapdocker
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker run --privileged -v /var/lib/docker:/var/lib/docker -v `pwd`/mount/:/host -t -i docker-host /host/wrapdocker
+COMMAND=${1-"/sbin/my_init --enable-insecure-key"}
+docker run --privileged -v /var/lib/docker:/var/lib/docker -v `pwd`/mount/:/host -t -i docker-host $COMMAND
 
