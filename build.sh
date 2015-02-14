9a7c2847a1d4b402011376374f269d73c0ffa7a8
Steve Strong <steve@srstrong.com>
2015-02-14 22:37:37 +0100

    Support for OSX

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ cd ${0%/*}
 TMPDIR=$(mktemp -d /tmp/XXXXXXXX)
 CIDFILE=$TMPDIR/cid
 PWD=$(pwd)
-PWD_ON_HOST="/home/id3as"${PWD#$HOME}
+PWD_ON_HOST=${ROOT_DIR_ON_HOST:="/"}${PWD}
 PROVISION_CMD="/host/provision-docker-host.sh"
 BASE_IMAGE="id3as-baseimage:0.0.1"
 IMAGE_NAME="docker-host"
 
-docker run --cidfile=$CIDFILE -t -v $PWD/mount/:/host:ro $BASE_IMAGE /bin/bash -c $PROVISION_CMD
+docker run --cidfile=$CIDFILE -t -v $PWD_ON_HOST/mount/:/host:ro $BASE_IMAGE /bin/bash -c $PROVISION_CMD
 
 CID=$(cat $CIDFILE)
 docker rmi $IMAGE_NAME >/dev/null 2>&1 || echo -n
